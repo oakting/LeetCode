7e7c8dd416372e0f8275be14972200b06628280b
oakting <oakting@gmail.com>
2015-09-19 22:45:08 -0700

    initial commit for LeetCode project

---
+++ LeetCode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LeetCode
+//
+//  Created by Hans Ting on 9/19/15.
+//  Copyright (c) 2015 Hans Ting. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
