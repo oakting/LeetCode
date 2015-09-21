37ae5bf8e9b8047fbf1e23c8ea135eaf28f72f93
oakting <oakting@gmail.com>
2015-09-20 21:45:25 -0700

    002_addTwoNumbers

---
+++ 002_addTwoNumbers/002_addTwoNumbers.cpp
@@ -0,0 +1,53 @@
+//
+//  002_addTwoNumbers.cpp
+//  LeetCode
+//
+
+#include <stdio.h>
+#include "Common.h"
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode *head = NULL;
+        ListNode **curr = &head;
+        int carry = 0;
+        while (l1 || l2) {
+            int val1 = 0, val2 = 0;
+            
+            val1 = getNextVal(l1);
+            val2 = getNextVal(l2);
+            
+            int digit = val1 + val2 + carry;
+            ListNode *newDigit = new ListNode(digit % 10);
+            carry = digit / 10;
+            
+            *curr = newDigit;
+            curr = &(newDigit->next);
+        }
+        
+        if (carry)
+            *curr = new ListNode(carry);
+        
+        return head;
+    }
+    
+private:
+    int getNextVal(ListNode* &l) {
+        int val = 0;
+        if (l != NULL) {
+            val = l->val;
+            l = l->next;
+        }
+        
+        return val;
+    }
+};
---
+++ LeetCode/Common.h
@@ -0,0 +1,18 @@
+//
+//  Common.h
+//  LeetCode
+//
+//  Created by Oak on 9/20/15.
+//  Copyright (c) 2015 Hans Ting. All rights reserved.
+//
+
+#ifndef LeetCode_Common_h
+#define LeetCode_Common_h
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+#endif
