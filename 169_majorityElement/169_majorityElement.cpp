d86c50dc6094350670a080706daddb47fe288fe1
oakting <oakting@gmail.com>
2015-09-22 15:46:29 -0700

    169_majorityElement.cpp

---
+++ 169_majorityElement/169_majorityElement.cpp
@@ -0,0 +1,57 @@
+//
+//  169_majorityElement.cpp
+//  LeetCode
+//
+//  Created by Oak on 9/22/15.
+//
+
+#include <stdio.h>
+#include <vector>
+#include <unordered_map>
+
+using std::vector;
+using std::unordered_map;
+
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int, int> map;
+        int major_lim = nums.size() / 2;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (map.find(nums[i]) == map.end()) {
+                map[nums[i]] = 1;
+            } else {
+                map[nums[i]] = map[nums[i]] + 1;
+            }
+            
+            if (map[nums[i]] > major_lim) return nums[i];
+        }
+        
+        return 0;
+    }
+    
+    // Moore Voting Algorithm
+    // Refer to:
+    // http://www.cs.utexas.edu/~moore/best-ideas/mjrty/index.html
+    int majorityElement(int* nums, int numsSize) {
+        int majority;
+        int count = 0;
+        int limit = numsSize / 2;
+        
+        for (int i = 0; i < numsSize; ++i) {
+            if (count == 0) {
+                majority = nums[i];
+                ++count;
+            } else {
+                if (majority == nums[i]) {
+                    ++count;
+                    if (count > limit) return majority;
+                }
+                else --count;
+            }
+        }
+        
+        return majority;
+    }
+};
