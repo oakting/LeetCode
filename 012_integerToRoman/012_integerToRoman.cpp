32bd50aab7543f5957219a9e477a9f09aee9fdc0
oakting <oakting@gmail.com>
2015-09-22 01:04:03 -0700

    012_integerToRoman

---
+++ 012_integerToRoman/012_integerToRoman.cpp
@@ -0,0 +1,74 @@
+//
+//  012_integerToRoman.cpp
+//  LeetCode
+//
+//  Created by Oak on 9/22/15.
+//
+
+#include <stdio.h>
+#include <string>
+
+using std::string;
+
+// 1        - I
+// 5        - V
+// 10       - X
+// 50       - L
+// 100      - C
+// 500      - D
+// 1000     - M
+
+
+class Solution {
+public:
+    string intToRoman(int num) {
+        string result;
+        int base = 1;
+        int tmp = num;
+        while (tmp / 10) {
+            base *= 10;
+            tmp /= 10;
+        }
+        
+        while (num) {
+            int digit = num / base;
+            
+            result = result + digitToRoman(digit, base);
+            num = (num % base);
+            base /= 10;
+        }
+        
+        return result;
+    }
+private:
+    string digitToRoman(int digit, int base) {
+        char *romanTable[4] = {"IVX", "XLC", "CDM", "M??"};
+        int tableIdx = -1;
+        
+        if (digit == 0) return "";
+        
+        while (base) {
+            base = base / 10;
+            tableIdx++;
+        }
+        
+        string romanStr;
+        if (digit <= 3) {
+            romanStr = string(digit, romanTable[tableIdx][0]);
+        } else if (digit <= 4) {
+            romanStr = string(1, romanTable[tableIdx][0]) + string(1, romanTable[tableIdx][1]);
+        } else if (digit <= 8) {
+            romanStr = string(1, romanTable[tableIdx][1]);
+            romanStr += string((digit - 5), romanTable[tableIdx][0]);
+        }
+        else if (digit == 9) {
+            romanStr = string(1, romanTable[tableIdx][0]) + string(1, romanTable[tableIdx][2]);
+        }
+        else if (digit == 10) {
+            romanStr = string(1, romanTable[tableIdx][2]);
+        }
+        
+        return romanStr;
+        
+    }
+};
