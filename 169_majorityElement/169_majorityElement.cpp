3b821e63cb4df01d8db5e2dca74202e70245d8fa
oakting <oakting@gmail.com>
2015-09-22 17:33:54 -0700

    169_majorityElement

--- 169_majorityElement/169_majorityElement.cpp
+++ 169_majorityElement/169_majorityElement.cpp
@@ -5,6 +5,12 @@
 //  Created by Oak on 9/22/15.
 //
 
+/*
+ Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+ 
+ You may assume that the array is non-empty and the majority element always exist in the array.
+ */
+
 #include <stdio.h>
 #include <vector>
 #include <unordered_map>
